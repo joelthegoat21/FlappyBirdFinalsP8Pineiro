274f46eb6a06cab36e037d3350adf6f0ef679741
joelthegoat21 <729037@student.dusd.net>
2024-12-13 15:41:40 -0800

    flappy bird
    i finished my flappy bird final

---
+++ P8FlappyFinalPineiro/Assets/Scripts/Column.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Column : MonoBehaviour
+{
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.GetComponent<Bird> () != null)
+        {
+            GameControl.instance.BirdScored();
+        }
+    }
+}
--- P8FlappyFinalPineiro/Assets/Scripts/GameControl.cs
+++ P8FlappyFinalPineiro/Assets/Scripts/GameControl.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameControl : MonoBehaviour
 {
 
     public static GameControl instance;
     public GameObject gameOverText;
+    public Text scoreText;
     public bool gameOver = false;
     public float scrollspeed = -1.5f;
+
+    private int score = 0;
 
     public float scrollSpedd { get; internal set; }
     // Start is called before the first frame update
@@ -30,9 +34,19 @@ public class GameControl : MonoBehaviour
     void Update()
     {
        if (gameOver == true && Input.GetMouseButtonDown(0))
-        {
+       {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        } 
+       } 
+    }
+
+    public void BirdScored()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+        score++;
+        scoreText.text = "Score: " + score.ToString();
     }
 
     public void BirdDied()
